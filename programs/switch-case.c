431ebd3b665d8ef5947ae343e8eecbc9fc495fd7
Suranjan <suranjanbiswas2002@gmail.com>
2021-04-07 21:41:11 +0530

    Completed the logic for switch-case

---
+++ programs/switch-case.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+  char grade;
+  printf("Please enter your grade!: \n");
+  scanf("%c", &grade);
+  switch (grade)
+  {
+  case 'A':
+    printf("You just aced the test\n");
+    break;
+  case 'B':
+    printf("You did good, ace it next time!\n");
+    break;
+  case 'C':
+    printf("Neither too good, nor to bad. But, you should work harder!\n");
+    break;
+  case 'D':
+    printf("You just barely passed.Study more!\n");
+    break;
+  case 'E':
+    printf("You got barely saved from getting the last rank!\n");
+    break;
+  case 'F':
+    printf("So! last. Just study rigorously and do better.\n");
+    break;
+  default:
+    printf("Show your grades to MUSK cause he deals with martians\n");
+  }
+}
