df4882d34dcc64a08c0e537a63c40026ca554019
Suranjan <suranjanbiswas2002@gmail.com>
2021-04-11 20:10:55 +0530

    test

--- advanced-C/pointers.c
+++ advanced-C/pointers.c
@@ -4,7 +4,7 @@ int main()
 {
   int slices = 15;
 
-  int *p = &slices; // pointer creation
+  int *p = &slices; // pointer
 
   printf("Before changing the value of the pointer: %i \n", slices);
   *p = 10;
